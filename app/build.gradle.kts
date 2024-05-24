bbe958676d97c3eccf8a7484036bc132fa069f8d
KVV <kvvpost@gmail.com>
2024-05-24 16:11:37 +0300

    1 Step

---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+	java
+	application
+	checkstyle
+	jacoco
+	id("org.springframework.boot") version "3.2.7-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "hexlet.code"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql:42.6.0")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+	reports {
+		xml.required = true
+	}
+}
