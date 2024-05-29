6c873e1d12844d2aa0bdaa0bb82bf09aad192e8c
KVV <kvvpost@gmail.com>
2024-05-29 19:39:33 +0300

    3 Step - without tests

--- src/test/java/hexlet/code/AppApplicationTests.java
+++ src/test/java/hexlet/code/AppApplicationTests.java
@@ -1,13 +1,36 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AppApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	// Технически имя тестового метода не важно
+	// Лучше использовать шаблон testНазваниеМетодаКонтроллера для основного сценария
+	@Test
+	public void testIndex() throws Exception {
+		mockMvc.perform(get("/users"))
+				.andExpect(status().isOk());
 	}
 
 }
