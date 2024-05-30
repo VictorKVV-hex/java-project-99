474f0271a698e8a81841cf393eb4499760697e3f
KVV <kvvpost@gmail.com>
2024-05-30 18:23:17 +0300

    4 Step_3

--- src/test/java/hexlet/code/AppApplicationTests.java
+++ src/test/java/hexlet/code/AppApplicationTests.java
@@ -1,28 +1,28 @@
 package hexlet.code;
 
 import hexlet.code.repository.UserRepository;
-import hexlet.code.model.User;
-import java.util.HashMap;
+//import hexlet.code.model.User;
+//import java.util.HashMap;
 
 import net.datafaker.Faker;
-import org.instancio.Select;
+//import org.instancio.Select;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+//import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+/*import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;*/
 //import org.junit.jupiter.api.Test;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 //import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.test.web.servlet.MockMvc;
-import org.instancio.Instancio;
+/*import org.instancio.Instancio;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;*/
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
