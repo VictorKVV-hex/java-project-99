736e31515c4a1ee1f6ae008b8d04ed0d20864477
KVV <kvvpost@gmail.com>
2024-06-03 18:40:59 +0300

    8.1 Step

--- src/test/java/hexlet/code/TaskControllerTest.java
+++ src/test/java/hexlet/code/TaskControllerTest.java
@@ -22,7 +22,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
 import java.util.Set;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
