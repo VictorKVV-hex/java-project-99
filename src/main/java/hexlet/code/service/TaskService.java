64aeddef4419c9ee5e34427b0e156c962bdfb89a
KVV <kvvpost@gmail.com>
2024-06-11 13:24:36 +0300

    2/4.4 Check

--- src/main/java/hexlet/code/service/TaskService.java
+++ src/main/java/hexlet/code/service/TaskService.java
@@ -8,8 +8,10 @@ import hexlet.code.dto.TaskUpdateDto;
 import hexlet.code.exception.ResourceNotFoundException;
 import hexlet.code.mapper.TaskMapper;
 //import hexlet.code.model.Label;
+import hexlet.code.model.Label;
 import hexlet.code.model.Task;
 //import hexlet.code.model.TaskStatus;
+import hexlet.code.model.TaskStatus;
 import hexlet.code.model.User;
 import hexlet.code.repository.TaskStatusRepository;
 import hexlet.code.repository.TaskRepository;
@@ -23,6 +25,7 @@ import org.springframework.stereotype.Service;
 //import java.util.HashSet;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 //import java.util.Objects;
 
 @Service
@@ -87,7 +90,7 @@ public class TaskService {
         return taskDTO;
     }
 
-    public TaskDto update(TaskUpdateDto dto, Long id) {
+/*    public TaskDto update(TaskUpdateDto dto, Long id) {
         var task = taskRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Задача с id " + id + " не найдена"));
         taskMapper.update(dto, task);
@@ -99,25 +102,53 @@ public class TaskService {
 
     private void setAssociations(TaskUpdateDto taskRequest, Task task) {
 
-/*        if (taskRequest.getDescription() != null) {
+*//*        if (taskRequest.getDescription() != null) {
             task.setDescription(taskRequest.getDescription().get());
         }
         TaskStatus taskStatus = null;
         if (taskRequest.getStatus() != null) {
             taskStatus = taskStatusRepository.findBySlug(taskRequest.getStatus().get()).orElseThrow();
-        }*/
+        }*//*
         // Почему маппер не меняет User, не понятно. Но так - работет!
         User user = null;
         if (taskRequest.getAssignee_id() != null) {
             user = userRepository.findById(taskRequest.getAssignee_id().get()).orElseThrow();
         }
-/*        List<Label> labels = null;
+*//*        List<Label> labels = null;
         if (taskRequest.getTaskLabelIds() != null) {
             labels = labelRepository.findAllById(taskRequest.getTaskLabelIds().get());
         }
-        task.setTaskStatus(taskStatus);*/
+        task.setTaskStatus(taskStatus);*//*
         task.setAssignee(user);
 //        task.setLabels(labels != null ? new HashSet<>(labels) : new HashSet<>());
+    }*/
+
+    public TaskDto update( TaskUpdateDto data, Long taskId) {
+        var task = taskRepository.findById(taskId)
+                .orElseThrow(() -> new ResourceNotFoundException("Task with id: " + taskId + " not found."));
+
+        taskMapper.update(data, task);
+
+        User assignee = null;
+        if (data.getAssignee_id() != null) {
+            assignee = userRepository.findById(data.getAssignee_id().get()).orElse(null);
+        }
+        task.setAssignee(assignee);
+
+        TaskStatus taskStatus = null;
+        if (data.getStatus() != null) {
+            taskStatus = taskStatusRepository.findBySlug(data.getStatus().get()).orElse(null);
+            task.setTaskStatus(taskStatus);
+        }
+
+        Set<Label> labelSet = null;
+        if (data.getTaskLabelIds() != null) {
+            labelSet = labelRepository.findByIdIn((data.getTaskLabelIds()).get()).orElse(null);
+        }
+        task.setLabels(labelSet);
+
+        taskRepository.save(task);
+        return taskMapper.map(task);
     }
 
     public void delete(Long id) {
