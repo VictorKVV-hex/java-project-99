fbd63389476afdd137c8d05512ea0c88674d661f
KVV <kvvpost@gmail.com>
2024-06-11 14:51:14 +0300

    2/4.5 Check

--- src/main/java/hexlet/code/service/TaskService.java
+++ src/main/java/hexlet/code/service/TaskService.java
@@ -23,7 +23,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 //import java.util.HashSet;
-import java.util.HashSet;
+//import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 //import java.util.Objects;
