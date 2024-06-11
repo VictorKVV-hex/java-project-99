5149ab6c29ae3e962774289a1f9730ac1c76a8cd
KVV <kvvpost@gmail.com>
2024-06-11 12:48:41 +0300

    2/4.3 Check

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
 	application
 	checkstyle
@@ -80,4 +83,16 @@ sentry {
 
 tasks.sentryBundleSourcesJava {
 	enabled = System.getenv("SENTRY_AUTH_TOKEN") != null
+}
+
+tasks.test {
+	useJUnitPlatform()
+	//https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+	testLogging {
+		exceptionFormat = TestExceptionFormat.FULL
+		events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+		showStackTraces = true
+		showCauses = true
+		showStandardStreams = true
+	}
 }
